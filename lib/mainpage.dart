bf253f5d523bf4a51ddae8ff785967e0d8c0cd20
Bogdan Nedelcu <bogdan.nedelcu@teamnet.ro>
2018-09-08 15:15:46 +0300

    Initial

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:kiosk/category.dart';
-import 'package:image_picker/image_picker.dart';
 import 'dart:async';
 import 'dart:io';
 import 'dart:convert';
